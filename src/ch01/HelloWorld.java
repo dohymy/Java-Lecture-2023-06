2757d49232eeba817d8217e08ed91114c2ead8d8
Do Hyun Myung <dohymywork@gmail.com>
2023-06-20 17:36:56 +0900

    1day

--- src/ch01/HelloWorld.java
+++ src/ch01/HelloWorld.java
@@ -1,11 +1,18 @@
 package ch01;
 
-public class HelloWorld {
+import java.util.Iterator;
 
+public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!!!");
 		System.out.println("안녕하세요?");
+		int[] array = new int[5];
+		for (int i = 0; i < array.length; i++) {
+			array[i]=i*10;
+		}
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
 	}
-
 }
